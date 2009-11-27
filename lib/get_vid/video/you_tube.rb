15be15daedbc2751c5ee686f63042ac2e5eefeef
Dave Giunta <dgiunta@mac.com>
2009-11-27 14:12:49 -0600

    [master] Updating the YouTube class to grab the download_link properly from the source.

--- lib/get_vid/video/you_tube.rb
+++ lib/get_vid/video/you_tube.rb
@@ -22,11 +22,21 @@ module GetVid
       end
       
       def download_token
-        @download_token ||= (original_src/'script').to_s.match(/\&t\=([^\&]*)\&?/).captures[0]
+        scripts = (original_src/'script')
+        puts scripts.length
+        puts scripts.match(/\&t\=([^\&]*\&?)/).inspect
+        @download_token ||= html.match(/\&t\=([^\&]*)\&?/).captures[0]
       end
       
       def download_link
-        @download_link ||= "http://www.youtube.com/get_video?video_id=#{video_id}&t=#{download_token}&fmt=18"
+        begin
+          link = original_src.html.grep(/.*img\.src/)[0].match(/img\.src ?\= ?['"](.+)['"]/)
+          url = URI.parse(link.captures[0])
+          dl_link = ["http://", url.host, "/videoplayback?", url.query].join
+          @download_link ||= dl_link
+        rescue Exception => e
+          @download_link = "ERROR"
+        end
       end
       
       def formatted_filename
--- spec/get_vid/video/you_tube_spec.rb
+++ spec/get_vid/video/you_tube_spec.rb
@@ -5,7 +5,7 @@ module GetVid
     describe YouTube do
       before(:each) do
         @url = 'http://www.youtube.com/watch?v=LXXAHe4V_4s'
-        @youtube_src = Hpricot(fixture("youtube_source.html"))
+        @youtube_src = Hpricot(fixture("youtube_source_2.html"))
         
         @yt = YouTube.new(@url)
         @yt.stub!(:open).and_return(@youtube_src)
@@ -25,22 +25,14 @@ module GetVid
           @yt.send(:video_id).should == "LXXAHe4V_4s"
         end
         
-        it "should have a download_token" do
-          @yt.send(:download_token).should == "vjVQa1PpcFNDJwlyL7C73FGaxFTbOTWaBG1SEW4TuBw%3D"
-        end
-        
         it "should add the contributor to the id3 tags" do
           @yt.send(:formatted_id3_tags).should include("--ta 'mostafahendry'")
         end
       end
       
-      context "getting the url to the downloadable movie file" do
-        it "should not use keepvid.com" do
-          @yt.send(:download_link).should_not include("keepvid.com")
-        end
-      
-        it "should grab the &t= value from the youtube source" do
-          @yt.send(:download_link).should == "http://www.youtube.com/get_video?video_id=LXXAHe4V_4s&t=vjVQa1PpcFNDJwlyL7C73FGaxFTbOTWaBG1SEW4TuBw%3D&fmt=18"
+      context "using the embeded download link." do
+        it "should properly set the download link variable" do
+          @yt.send(:download_link).should == "http://v22.lscache6.c.youtube.com/videoplayback?ip=0.0.0.0&sparams=id%2Cexpire%2Cip%2Cipbits%2Citag%2Calgorithm%2Cburst%2Cfactor&fexp=905301%2C904804&algorithm=throttle-factor&itag=34&ipbits=0&burst=40&sver=3&expire=1259370000&key=yt1&signature=296B483065D2C5322CD47AD0796FAE70A6AE6819.8A381F9E53B9EB52932A99C1075E42555DED988B&factor=1.25&id=2d75c01dee15ff8b"
         end
       end
     end
